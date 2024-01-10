8a98bce73f4f9aed496f8ec21ef167615857be4b
Rio puji nugroho <142971502+MJavs@users.noreply.github.com>
2024-01-10 18:31:57 +0700

    Add files via upload

---
+++ pertemuan2/pengkondisian.php
@@ -0,0 +1,19 @@
+<?php
+$nilai = 80;
+if($nilai <= 85 && $nilai !== "80"){
+    echo "benar";
+}else if($nilai == 85){
+    echo "nilainya adalah 85 ";
+}else{
+    echo "salah";
+}
+
+
+// latihan
+$harga = 20000;
+if($harga > 50000){
+    echo "<h1 style= 'color: blue'>harganya di atas 50rb</h1>";
+}else if{
+    echo "<h1 style ='color: red  '> harganya di bawah 50rb</h1>";
+}
+?>
---
+++ pertemuan2/pengulangan.php
@@ -0,0 +1,25 @@
+<?php
+// for
+// while
+// do while
+// foreach
+
+// langkah - langkah melakukan pengulangan
+// 1. Menentukan nilai awal
+// 2. menentukan kondisi terminasi
+// 3. menuliskan increment/decrement
+
+
+// for
+for($nilaiAwal = 1; $nilaiAwal <= 10; $nilaiAwal++){
+    echo "Angka ke - $nilaiAwal <br/>"; 
+}
+
+// while
+
+$noAngkot = 1;
+while($noAngkot <= 25 ){
+    echo  "Angkot ke - $noAngkot <br/>";
+    $noAngkot++;
+}
+?>
